e7dbe6acd093656c4b32bdc6b03bf322509aad8b
pussik <vlad.hobta79@mail.ru>
2022-10-05 15:01:31 +0300

    improving responses

--- JobPortal/apps/response/serializers.py
+++ JobPortal/apps/response/serializers.py
@@ -1,18 +1,18 @@
 from rest_framework import serializers
-from .models import ResponseToVacancy, ResponseToCandidate
+from .models import AnswerToVacancy, AnswerToCandidate
 
 
-class ResponseToVacancySerializer(serializers.ModelSerializer):
+class AnswerToVacancySerializer(serializers.ModelSerializer):
     candidate_first_name = serializers.CharField(source='candidate.first_name', read_only=True)
     candidate_last_name = serializers.CharField(source='candidate.last_name', read_only=True)
     vacancy_title = serializers.CharField(source='vacancy.title', read_only=True)
 
     class Meta:
-        model = ResponseToVacancy
+        model = AnswerToVacancy
         fields = '__all__'
 
     def validate(self, data):
-        if ResponseToVacancy.objects.get(candidate=data['candidate'], vacancy=data['vacancy']):
+        if AnswerToVacancy.objects.get(candidate=data['candidate'], vacancy=data['vacancy']):
             raise serializers.ValidationError('This candidate already had replied to this vacancy')
         return data
     # def validate_candidate(self, value):
@@ -23,9 +23,23 @@ class ResponseToVacancySerializer(serializers.ModelSerializer):
     #      if ResponseVacancy.objects.get
 
 
-class ResponseToCandidateSerializer(serializers.ModelSerializer):
+class AnswerToCandidateSerializer(serializers.ModelSerializer):
+    candidate_pk = serializers.CharField(source="answer_to_vacancy.candidate.pk", read_only=True)
 
     class Meta:
-        model = ResponseToCandidate
+        model = AnswerToCandidate
         fields = '__all__'
 
+    def create(self, validated_data):
+        vd = validated_data
+        print(vd['answer_to_vacancy'])
+        candidate_pk = AnswerToVacancy.objects.get(pk=vd['answer_to_vacancy'].pk).candidate
+        answer = AnswerToCandidate.objects.create(
+            answer_to_vacancy=vd['answer_to_vacancy'],
+            candidate=candidate_pk,
+            message=vd['message'],
+            decision=vd['decision'],
+        )
+        answer.save()
+
+        return answer
--- JobPortal/apps/response/urls.py
+++ JobPortal/apps/response/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ResponseVacancyView, ResponseCandidateListView, ResponseCompanyListView, ResponseVacancyListView, ResponseToCandidateView
+from .views import AnswerVacancyView, AnswerCandidateListView, AnswerCompanyListView, AnswerVacancyListView, \
+    AnswerToCandidateView, AnswersToCandidateListView
 
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -7,12 +8,13 @@ from rest_framework.authtoken.views import obtain_auth_token
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
     # responses to vacancy
-    path('', ResponseVacancyView.as_view()),
-    path('<int:pk_response>', ResponseVacancyView.as_view()),
-    path('candidate=<int:pk_candidate>', ResponseCandidateListView.as_view()),
-    path('company=<int:pk_company>', ResponseCompanyListView.as_view()),
-    path('vacancy=<int:pk_vacancy>', ResponseVacancyListView.as_view()),
-    # responses to candidate's responses
-    path('answer_to=<int:pk_response>', ResponseToCandidateView.as_view()),
+    path('', AnswerVacancyView.as_view()),
+    path('<int:pk_answer>', AnswerVacancyView.as_view()),
+    path('candidate=<int:pk_candidate>', AnswerCandidateListView.as_view()),
+    path('company=<int:pk_company>', AnswerCompanyListView.as_view()),
+    path('vacancy=<int:pk_vacancy>', AnswerVacancyListView.as_view()),
+    # answers to candidate's answers
+    path('answer_to=<int:pk_answer>', AnswerToCandidateView.as_view()),
     # path(''),
+    path('answers_to_candidate=<int:pk_candidate>', AnswersToCandidateListView.as_view()),
 ]
